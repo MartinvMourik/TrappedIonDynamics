98f86f1709582de7ee40bddf0ef797ae923ba681
Martin van Mourik <martin.van-mourik@uibk.ac.at>
2019-05-29 21:02:37 +0200

    -

--- main.m
+++ main.m
@@ -2,16 +2,7 @@ clear
 addpath('functions\');
 
 % Select ion chain
-<<<<<<< .mine
 ions = {'Ca','Sr','Ca'};
-
-=======
-<<<<<<< Updated upstream
-ions = {'Ca','Ca','Ca'};
->>>>>>> .theirs
-=======
-ions = {'Ca'};
->>>>>>> Stashed changes
 
 % Choose the file that defines the RF potential. This is a vector that
 % contains 25 values, corresponding to the first 25 spherical harmonic
@@ -24,7 +15,6 @@ settings.rf_multipoles = multipoles;
 
 % The following settings need to be set
 settings.coulomb = 1; %Boolean. Turn coulomb potential on or off
-<<<<<<< Updated upstream
 settings.rf_voltage = 100;
 settings.rf_frequency = 35e6;
 settings.rf_phase = 0;
@@ -36,21 +26,6 @@ settings.precool = 0; % Boolean. Does a non-physical damping, to help get ions i
 settings.precool_str = [1e6;1e6;1e6];
 settings.precool_time = 1e-5;
 ion_positions = [-3.35,0,3.35]*1e-6; % Initial positions along trap axis
-=======
-settings.rf_voltage = 50;
-settings.rf_frequency = 35e6; %Frequency in Hz
-settings.rf_phase = 0; % Phase at t=0;
-settings.duration = 1e-3; 
-settings.time_step = 1e-9; % Not a simulation step, but returned values
-settings.fields = [0,0,0]; % Field potentials in V/m
-settings.curvatures = [0,0,1.2e7,0,1e6]; % Curvatures in V/m^2. 3rd term is endcap-like confining potential. 5th provides radial degeneracy
-settings.precool = 0; % Boolean. Does a non-physical damping, to help get ions in equilibrium positions
-settings.precool_str = [1e6;1e6;1e6];
-settings.precool_time = 1e-5;
-axis_positions = 0;%[-2e-6,0e-6,2e-6];%[-2,2]*1e-6; % Initial positions along trap axis
-ion_speeds = [0,0,0];
->>>>>>> Stashed changes
-
 % The following finds the RF minimum point of the chosen multipole file.
 % The DC spherical harmonics are build around this point.
 minfunc = @(x)sum((get_rf_gradients(x,settings.rf_multipoles).^2));
@@ -70,16 +45,8 @@ for i = 1:length(ions)
     settings.ions(i).coupling = 0*25*2*pi*1e6;
     settings.ions(i).detuning = -22*2*pi*1e6;
 end
-<<<<<<< .mine
 settings.ions(1).start_vel = [1,1,0]*50;
 
-=======
-<<<<<<< Updated upstream
-
->>>>>>> .theirs
-=======
-%settings.ions(1).start_vel = [1,1,0]*200;
->>>>>>> Stashed changes
 % Starts scan
 [t,y] = IonTrajectory_function(settings);
 % y is a vector that contains [3 positions, 3 velocities, 3 positions, 3
